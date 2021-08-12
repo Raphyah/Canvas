179fd07dea46fdeab8b9916cb361ba670f15843e
Raphyah <45445230+Raphyah@users.noreply.github.com>
2021-08-12 09:44:08 -0300

    Added add(), render(), width() and height() properties.
    Added new functions for adding and rendering the objects in screen, and also getters for width and height of canvas.

--- build/Canvas.js
+++ build/Canvas.js
@@ -20,4 +20,24 @@ class Canvas {
 			height : this.dom.height;
 		this.context.clearRect(x, y, width, height);
 	}
+
+	add(object){
+		this.objects.push(object);
+		object.inserted_in(this);
+	}
+
+	render(){
+		for(let x in this.objects){
+			if(typeof this.objects[x] === "function")
+				this.objects[x].update();
+		}
+	}
+
+	get width(){
+		return this.dom.width;
+	}
+	
+	get height(){
+		return this.dom.height;
+	}
 }
