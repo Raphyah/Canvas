f41cd85e21cf5214ab7ba1c456790f0835a4fb5e
Raphyah <45445230+Raphyah@users.noreply.github.com>
2021-08-12 09:56:56 -0300

    Commented constructors and added new setters and classes.

--- build/Canvas.js
+++ build/Canvas.js
@@ -1,4 +1,6 @@
+// The Canvas constructor
 class Canvas {
+	// Initialize object
 	constructor(ctx){
 		this.objects = [];
 
@@ -6,11 +8,13 @@ class Canvas {
 		this.context = this.dom.getContext(ctx);
 	}
 
+	// Set canvas DOM dimensions
 	setSize(width, height){
-		this.dom.width = width;
-		this.dom.height = height;
+		this.width = width;
+		this.height = height;
 	}
 
+	// Clears the canvas object
 	clear(x, y, width, height){
 		x = x.constructor === Number ? x : 0;
 		y = y.constructor === Number ? y : 0;
@@ -21,11 +25,13 @@ class Canvas {
 		this.context.clearRect(x, y, width, height);
 	}
 
+	// Add a new object
 	add(object){
 		this.objects.push(object);
 		object.inserted_in(this);
 	}
 
+	// Render every object added to canvas
 	render(){
 		for(let x in this.objects){
 			if(typeof this.objects[x] === "function")
@@ -33,11 +39,33 @@ class Canvas {
 		}
 	}
 
+	// Getter for canvas width
 	get width(){
 		return this.dom.width;
 	}
+
+	// Setter for canvas width
+	set width(value){
+		if(value.constructor === Number)
+			this.dom.width = value;
+		else
+			console.warn("width() value is not a Number.");
+	}
 	
+	// Getter for canvas height
 	get height(){
 		return this.dom.height;
 	}
+
+	// Setter for canvas height
+	set height(value){
+		if(value.constructor === Number)
+			this.dom.height = value;
+		else
+			console.warn("height() value is not a Number.");
+	}
+}
+
+Canvas.Rect = class {
+
 }
