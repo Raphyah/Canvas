78a6896b4767b8c8d84a6d6aebbde9faaaba7cb6
Raphyah <45445230+Raphyah@users.noreply.github.com>
2021-08-13 16:02:46 -0300

    Updated main.js

--- example/script/main.js
+++ example/script/main.js
@@ -1,11 +1,24 @@
-window.addEventListener("error", e => {
-	cancelAnimationFrame(game_update);
-});
+'use strict';
 
 const canvas = new Canvas();
 canvas.setSize(1280,720);
+document.body.appendChild(canvas.dom);
 
-function update(){
+const rect = new Canvas.Rect(10,10,10,10);
+canvas.add(rect);
+canvas.render();
+
+function animate(ts){
+	rect.x = ts / 100;
+}
+function update(ts){
+	canvas.clear();
+	animate(ts);
+	canvas.render();
 	game_update = requestAnimationFrame(update);
 }
 let game_update = requestAnimationFrame(update);
+
+window.addEventListener("error", e => {
+	cancelAnimationFrame(game_update);
+});
