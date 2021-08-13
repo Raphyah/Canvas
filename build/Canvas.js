1753a1876fb8c278ca70787dd7100f1803205593
Raphyah <45445230+Raphyah@users.noreply.github.com>
2021-08-13 15:50:08 -0300

    Canvas.js is now ES6.

--- build/Canvas.js
+++ build/Canvas.js
@@ -1,18 +1,3 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
     if (kind === "m") throw new TypeError("Private method is not writable");
     if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
@@ -31,9 +16,9 @@ Object.defineProperty(Object.prototype, "ancestors", {
      *
      * @returns An array with every descendant of this object.
      */
-    get: function () {
-        var arr = [];
-        var p = this.constructor.prototype;
+    get() {
+        const arr = [];
+        let p = this.constructor.prototype;
         while (true) {
             if (!p)
                 break;
@@ -50,19 +35,18 @@ Object.defineProperty(Object.prototype, "is_a", {
      * @param {*} object Check if this is equal to object.
      * @returns a boolean indicating if is descendant of.
      */
-    value: function (object) {
+    value(object) {
         return this.ancestors.includes(object);
     }
 });
 // The Canvas constructor
-var Canvas = /** @class */ (function () {
+class Canvas {
     // Initialize object
     /**
      *
      * @param {String} ctx Sets the context type. (default: 2d)
      */
-    function Canvas(ctx) {
-        if (ctx === void 0) { ctx = "2d"; }
+    constructor(ctx = "2d") {
         this.objects = [];
         this.dom = document.createElement("canvas");
         this.context = this.dom.getContext(ctx);
@@ -73,10 +57,10 @@ var Canvas = /** @class */ (function () {
      * @param {Number} width Sets the canvas width.
      * @param {Number} height Sets the canvas height.
      */
-    Canvas.prototype.setSize = function (width, height) {
+    setSize(width, height) {
         this.width = width;
         this.height = height;
-    };
+    }
     // Clears the canvas object
     /**
      *
@@ -85,11 +69,7 @@ var Canvas = /** @class */ (function () {
      * @param {Number} width Sets the cleaning area witdh. Set to this.canvas.width if NaN.
      * @param {Number} height Sets the cleaning area height. Set to this.canvas.height if NaN.
      */
-    Canvas.prototype.clear = function (x, y, width, height) {
-        if (x === void 0) { x = false; }
-        if (y === void 0) { y = false; }
-        if (width === void 0) { width = false; }
-        if (height === void 0) { height = false; }
+    clear(x = false, y = false, width = false, height = false) {
         x = x.constructor === Number ? x : 0;
         y = y.constructor === Number ? y : 0;
         width = width.constructor === Number ?
@@ -97,78 +77,69 @@ var Canvas = /** @class */ (function () {
         height = height.constructor === Number ?
             height : this.dom.height;
         this.context.clearRect(x, y, width, height);
-    };
+    }
     // Add a new object
     /**
      *
      * @param {CanvasObject} object Append a new object to this one.
      */
-    Canvas.prototype.add = function (object) {
+    add(object) {
         if (!object.is_a(CanvasObject))
             return console.log("The object is not a CanvasObject.");
         this.objects.push(object);
         object.insertIn(this);
         console.log(this.objects);
-    };
+    }
     // Render every object added to canvas
     /**
      * Render every object appended to this one.
      */
-    Canvas.prototype.render = function () {
-        for (var x in this.objects) {
+    render() {
+        for (let x in this.objects) {
             if (this.objects[x].is_a(CanvasObject)) {
                 this.objects[x].update();
             }
         }
-    };
-    Object.defineProperty(Canvas.prototype, "width", {
-        // Getter for canvas width
-        /**
-         *
-         * @returns Get the current width of the canvas element.
-         */
-        get: function () {
-            return this.dom.width;
-        },
-        // Setter for canvas width
-        /**
-         *
-         * @param {Number} value Set a new width for canvas element.
-         */
-        set: function (value) {
-            if (value.constructor === Number)
-                this.dom.width = value;
-            else
-                console.warn("width() value is not a Number.");
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Canvas.prototype, "height", {
-        /**
-         *
-         * @returns {Number} Get the current height of the canvas element.
-         */
-        get: function () {
-            return this.dom.height;
-        },
-        /**
-         *
-         * @param {Number} value Set a new height for canvas element.
-         */
-        set: function (value) {
-            if (value.constructor === Number)
-                this.dom.height = value;
-            else
-                console.warn("height() value is not a Number.");
-        },
-        enumerable: false,
-        configurable: true
-    });
-    return Canvas;
-}());
+    }
+    // Getter for canvas width
+    /**
+     *
+     * @returns Get the current width of the canvas element.
+     */
+    get width() {
+        return this.dom.width;
+    }
+    // Setter for canvas width
+    /**
+     *
+     * @param {Number} value Set a new width for canvas element.
+     */
+    set width(value) {
+        if (value.constructor === Number)
+            this.dom.width = value;
+        else
+            console.warn("width() value is not a Number.");
+    }
+    /**
+     *
+     * @returns {Number} Get the current height of the canvas element.
+     */
+    get height() {
+        return this.dom.height;
+    }
+    /**
+     *
+     * @param {Number} value Set a new height for canvas element.
+     */
+    set height(value) {
+        if (value.constructor === Number)
+            this.dom.height = value;
+        else
+            console.warn("height() value is not a Number.");
+    }
+}
 // The CanvasObject constructor
-var CanvasObject = /** @class */ (function () {
+class CanvasObject {
     /** Create a new CanvasObject with the parameters bellow.
      *
      * @param {Number} x Sets the position x of the object in context to the Canvas origin/parent object.
@@ -176,7 +147,7 @@ var CanvasObject = /** @class */ (function () {
      * @param {Number} width Sets the width of the rectangle.
      * @param {Number} height Sets the height of the rectangle.
      */
-    function CanvasObject(x, y, width, height) {
+    constructor(x, y, width, height) {
         _CanvasObject_x.set(this, void 0);
         _CanvasObject_y.set(this, void 0);
         this.parent = false;
@@ -191,96 +162,74 @@ var CanvasObject = /** @class */ (function () {
      *
      * @param {CanvasObject} object Add a new object.
      */
-    CanvasObject.prototype.add = function (object) {
+    add(object) {
         this.objects.push(object);
         object.insertIn(this);
-    };
+    }
     /**
      *
      * @param {Canvas | CanvasObject} object The object for this to be inserted in.
      */
-    CanvasObject.prototype.insertIn = function (object) {
+    insertIn(object) {
         if (!object.is_a(Canvas) && !object.is_a(CanvasObject))
             return console.log("Not a CanvasObject.");
         this.parent = object;
-    };
-    Object.defineProperty(CanvasObject.prototype, "insertedIn", {
-        /**
-         *
-         * @returns {Canvas | CanvasObject} The parent object of this.
-         */
-        get: function () {
-            return this.parent;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(CanvasObject.prototype, "x", {
-        /**
-         *
-         * @returns {Number} The total x position of this.
-         */
-        get: function () {
-            var p = this.parent.is_a(Canvas.Rect) ? this.parent.x : 0;
-            return __classPrivateFieldGet(this, _CanvasObject_x, "f") + p;
-        },
-        /**
-         *
-         * @param {Number} x Sets the x position of the object accordingly to parent.
-         */
-        set: function (x) {
-            __classPrivateFieldSet(this, _CanvasObject_x, x, "f");
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(CanvasObject.prototype, "y", {
-        /**
-         *
-         * @returns {Number} The total y position of this.
-         */
-        get: function () {
-            var p = this.parent.is_a(Canvas.Rect) ? this.parent.y : 0;
-            return __classPrivateFieldGet(this, _CanvasObject_y, "f") + p;
-        },
-        /**
-         *
-         * @param {Number} y Sets the y position of the object accordingly to parent.
-         */
-        set: function (y) {
-            __classPrivateFieldSet(this, _CanvasObject_y, y, "f");
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(CanvasObject.prototype, "_x", {
-        /**
-         *
-         * @returns {Number} The raw x position of this.
-         */
-        get: function () {
-            return __classPrivateFieldGet(this, _CanvasObject_x, "f");
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(CanvasObject.prototype, "_y", {
-        /**
-         *
-         * @returns {Number} The raw y position of this.
-         */
-        get: function () {
-            return __classPrivateFieldGet(this, _CanvasObject_y, "f");
-        },
-        enumerable: false,
-        configurable: true
-    });
-    return CanvasObject;
-}());
+    }
+    /**
+     *
+     * @returns {Canvas | CanvasObject} The parent object of this.
+     */
+    get insertedIn() {
+        return this.parent;
+    }
+    /**
+     *
+     * @returns {Number} The total x position of this.
+     */
+    get x() {
+        const p = this.parent.is_a(Canvas.Rect) ? this.parent.x : 0;
+        return __classPrivateFieldGet(this, _CanvasObject_x, "f") + p;
+    }
+    /**
+     *
+     * @returns {Number} The total y position of this.
+     */
+    get y() {
+        const p = this.parent.is_a(Canvas.Rect) ? this.parent.y : 0;
+        return __classPrivateFieldGet(this, _CanvasObject_y, "f") + p;
+    }
+    /**
+     *
+     * @param {Number} x Sets the x position of the object accordingly to parent.
+     */
+    set x(x) {
+        __classPrivateFieldSet(this, _CanvasObject_x, x, "f");
+    }
+    /**
+     *
+     * @param {Number} y Sets the y position of the object accordingly to parent.
+     */
+    set y(y) {
+        __classPrivateFieldSet(this, _CanvasObject_y, y, "f");
+    }
+    /**
+     *
+     * @returns {Number} The raw x position of this.
+     */
+    get _x() {
+        return __classPrivateFieldGet(this, _CanvasObject_x, "f");
+    }
+    /**
+     *
+     * @returns {Number} The raw y position of this.
+     */
+    get _y() {
+        return __classPrivateFieldGet(this, _CanvasObject_y, "f");
+    }
+}
 _CanvasObject_x = new WeakMap(), _CanvasObject_y = new WeakMap();
 // The Canvas.Rect constructor
-Canvas.Rect = /** @class */ (function (_super) {
-    __extends(Rect, _super);
+Canvas.Rect = class extends CanvasObject {
     /** Create a new Rect using the parameters below
      *
      * @param {Number} x Sets the position x of the object in context to the Canvas origin.
@@ -292,9 +241,8 @@ Canvas.Rect = /** @class */ (function (_super) {
      * @param {String} style.color Defines the color of the Rect.
      *
      */
-    function Rect(x, y, width, height, style) {
-        if (style === void 0) { style = { color: "#000000" }; }
-        var _this = _super.call(this, x, y, width, height) || this;
+    constructor(x, y, width, height, style = { color: "#000000" }) {
+        super(x, y, width, height);
         if (!style || !style.constructor === Object) {
             style = {};
         }
@@ -305,20 +253,19 @@ Canvas.Rect = /** @class */ (function (_super) {
         style.type = style.type.toLowerCase();
         if (["fill", "rect"].includes(style.type))
             style.type = "fill";
-        _this.color = style.color;
-        _this.type = style.type;
-        return _this;
+        this.color = style.color;
+        this.type = style.type;
     }
     /** Update the object and every child
      *
      * @returns false if failed.
      */
-    Rect.prototype.update = function () {
+    update() {
         if (!this.parent)
             return false;
         if (!this.rendering_canvas) {
             if (this.is_a(Canvas) || this.is_a(CanvasObject)) {
-                var scanning = this.parent;
+                let scanning = this.parent;
                 while (true) {
                     //console.log(scanning);
                     if (scanning.is_a(Canvas)) {
@@ -335,13 +282,12 @@ Canvas.Rect = /** @class */ (function (_super) {
         }
         if (!this.rendering_canvas)
             return false;
-        var ctx = this.rendering_canvas.context;
+        let ctx = this.rendering_canvas.context;
         ctx.save();
         ctx[this.type + "Style"] = this.color;
         ctx[this.type + "Rect"](this.x, this.y, this.width, this.height);
         ctx.restore();
-        for (var x in this.objects)
+        for (let x in this.objects)
             this.objects[x].update();
-    };
-    return Rect;
-}(CanvasObject));
+    }
+};
